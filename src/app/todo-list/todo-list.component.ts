d0cfb8c7f46a12f0a67e42b625ffcd6e5cdaf04c
wehrling <fwehrling@yahoo.fr>
2017-11-28 22:59:54 +0100

    first commit

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../store';
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO, REMOVE_ALL_TODOS } from '../actions';
+import { ITodo } from '../todo';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  @select() todos;
+
+  model: ITodo = {
+    id: 0,
+    description: '',
+    responsible: '',
+    priority: 'low',
+    isCompleted: false
+  }
+
+  constructor(private ngRedux: NgRedux<IAppState>) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.ngRedux.dispatch({type: ADD_TODO, todo: this.model})
+  }
+
+  toggleTodo(todo) {
+    this.ngRedux.dispatch({type: TOGGLE_TODO, id: todo.id});
+  }
+
+  removeTodo(todo) {
+    this.ngRedux.dispatch({type: REMOVE_TODO, id: todo.id});
+  }
+}
